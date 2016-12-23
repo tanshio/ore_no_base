aa3ab907a190caca14d34edabfc1013ee66330b2
tanshio <rakuburomail+tanshio@gmail.com>
2016-12-24 05:07:23 +0900

    babel settings

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,16 @@
-const test = `${1+1}=2`
-const testPromise = ()=>{
-  return new Promise(function(resolve, reject) {
-    setTimeout(function(){
-      console.log("o-i");
-      resolve();
-    },2000)
-  });
-}
+async function test(){
+     // 60回Promiseを待機する。
+     // rejectされた時は、自動的にこの関数が中断されるからtry-catchで囲む必要もない。
+    for(let i=0;i<60;++i){
+         // yieldと書いてた部分をawaitにする
+         await delayAndRandom();
+    }
+ }
 
-testPromise().then(function(){console.log("hello")})
+test().then(()=>{
+     // 全てresolveが呼ばれた場合、この関数が実行される
+     console.log("全部成功したよ！");
+}).catch(()=>{
+     // 途中1回でもreject関数が呼ばれた場合、この関数が実行される
+     console.log("途中で失敗したみたい…");
+});
