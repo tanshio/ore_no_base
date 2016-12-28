21df46c6216351c1c00ebe4efe1a3cc0621939c1
tanshio <rakuburomail+tanshio@gmail.com>
2016-12-28 11:58:01 +0900

    Setting Pug

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-import 'babel-polyfill';
+// import 'babel-polyfill';
 async function test(){
      // 60回Promiseを待機する。
      // rejectされた時は、自動的にこの関数が中断されるからtry-catchで囲む必要もない。
@@ -14,7 +14,7 @@ function delayAndRandom(){
 
 test().then(()=>{
      // 全てresolveが呼ばれた場合、この関数が実行される
-     console.log("全部成功したよ！");
+     console.log("全部成功したよ!!！");
 }).catch(()=>{
      // 途中1回でもreject関数が呼ばれた場合、この関数が実行される
      console.log("途中で失敗したみたい…");
