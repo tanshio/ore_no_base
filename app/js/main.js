8023775830936c4d794a5124730d38f5193a39dc
tanshio <shotatanno@Shotas-iMac.local>
2017-03-29 20:04:14 +0900

    設定など

--- app/js/main.js
+++ app/js/main.js
@@ -9,12 +9,12 @@ async function test(){
  }
 
 function delayAndRandom(){
-
+  var aaa = "aaa";
 }
 
 test().then(()=>{
      // 全てresolveが呼ばれた場合、この関数が実行される
-     console.log("全部成功した");
+     console.log("全部成功しました。");
 }).catch(()=>{
      // 途中1回でもreject関数が呼ばれた場合、この関数が実行される
      console.log("途中で失敗したみたい…aaa");
