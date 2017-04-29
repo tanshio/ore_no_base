738a4c83fa1ddc4b55df166c376283a7ba95ad57
tanshio <rakuburomail+tanshio@gmail.com>
2017-04-29 17:16:29 +0900

    postcss settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,54 +1,54 @@
+const banner = `
+Theme Name: siteName
+Theme URI: https://.com/
+Author: tanshio
+Author URI: https://.com/
+Description: Description
+Version: 1.0.0
+License: GNU General Public License v2 or later
+License URI: LICENSE
+Text Domain: siteName
+Tags:
 
-// module.exports = {
-//   'use': [
-//     'postcss-import',
-//     'postcss-nesting',
-//     'autoprefixer',
-//     'postcss-flexbugs-fixes',
-//     'postcss-custom-properties',
-//     'postcss-custom-media',
-//     'postcss-font-magician',
-//     'css-mqpacker',
-//     'postcss-reporter',
-//     'postcss-browser-reporter',
-//     'cssnano'
-//   ],
-//   'postcss-import': {
-//     onImport: function(sources) {
-//       global.watchCSS(sources, this.from);
-//     }
-//   },
-//   'postcss-custom-properties': {
-//     preserve: false,
-//   },
-//   'postcss-font-magician': {
-//     foundries: ['custom', 'hosted', 'google']
-//   }
-// };
+This theme, like WordPress, is licensed under the GPL.
+Use it to make something cool, have fun, and share what you've learned with others.
 
-module.exports = {
-  plugins: [
-    require('postcss-import')(),
-    require('postcss-nesting')(),
-    require('postcss-custom-properties')({
-      preserve: false
-    }),
-    require('postcss-custom-media')(),
-    require('autoprefixer')()
-  ]
+benyland is based on Underscores http://underscores.me/, (C) 2012-2016 Automattic, Inc.
+Underscores is distributed under the terms of the GNU GPL v2 or later.
+
+Normalizing styles have been helped along thanks to the fine work of
+Nicolas Gallagher and Jonathan Neal http://necolas.github.io/normalize.css/
+`
+
+console.log(process.env.NODE_ENV)
+let plugins = [
+  require('postcss-import')(),
+  require('postcss-nesting')(),
+  require('postcss-custom-properties')({
+    preserve: false
+  }),
+  require('postcss-custom-media')(),
+  require('postcss-flexbugs-fixes')(),
+  require('autoprefixer')(),
+  require('postcss-pxtorem')({
+    rootValue: 16,
+    unitPrecision: 5,
+    propList: ['font', 'font-size'],
+    selectorBlackList: [],
+    replace: true,
+    mediaQuery: false,
+    minPixelValue: 0
+  })
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(require('cssnano')())
+  plugins.push(require('postcss-banner')({
+    banner: banner,
+    important: true
+  }))
 }
 
-// module.exports = (ctx) => ({
-//   parser: ctx.parser ? 'sugarss' : false,
-//   'map': 'inline',
-//   plugins: {
-//     'autoprefixer': {},
-//     'postcss-nesting': {},
-//     'postcss-import': { root: ctx.file.dirname },
-//     'postcss-custom-properties': {
-//       preserve: false
-//     },
-//     'postcss-nested': {},
-//     cssnano: ctx.env === 'production' ? {} : false
-//   }
-// })
+module.exports = {
+  plugins: plugins
+}
